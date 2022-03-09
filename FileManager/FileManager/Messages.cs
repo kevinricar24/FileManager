ca95fbe7d115db6c9b95c32112417d2040376547
Kevin Sejin <Kevin.Sejin@teaminternational.com>
2022-03-08 22:55:36 -0500

    add solution

---
+++ FileManager/FileManager/Messages.cs
@@ -0,0 +1,33 @@
+﻿namespace FileManager
+{
+    public class Messages
+    {
+        public const string titleApp = "File Manager!";
+
+        public const string directory = "Directory";
+        public const string mode = "Mode";
+        public const string lastWriteTime = "LastWriteTime";
+        public const string length = "Length";
+        public const string name = "Name";
+
+        public const string separator4 = "----";
+        public const string separator5 = "-----";
+        public const string separator6 = "------";
+        public const string separator7 = "-------";
+        public const string separator8 = "--------";
+        public const string separator9 = "---------";
+        public const string separator10 = "----------";
+        public const string separator11 = "-----------";
+        public const string separator12 = "------------";
+        public const string separator13 = "-------------";
+        public const string separator14 = "--------------";
+
+        public const string sectionDirectory = $"--------------- {directory} ---------------";
+        public const string sectionFolder = "--------------- Folder ---------------";
+        public const string sectionFile = "--------------- File ---------------";
+        public const string sectionExit = "--------------- Exit ---------------";
+
+
+
+    }
+}
